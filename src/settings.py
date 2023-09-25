5a50a158035622dc64fb9d29000a4c68607e6caa
erastov-alex <erastov.alex@gmail.com>
2023-09-25 15:32:50 +0300

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "RuCode Hand Segmentation 2021"
 PROJECT_NAME_FULL: str = "RuCode Hand Segmentation 2021"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
