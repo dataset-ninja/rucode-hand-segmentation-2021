8196718119fabeea33d3aa3257d69b494742955e
erastov-alex <erastov.alex@gmail.com>
2023-09-25 14:44:19 +0300

    upd settings? CITATION

--- CITATION.md
+++ CITATION.md
@@ -1,7 +1,12 @@
 If you make use of the RuCode Hand Segmentation 2021 data, please cite the following reference:
 
 ``` bibtex
-PASTE HERE CUSTOM CITATION FROM THE SOURCE URL
+@dataset{RuCode Hand Segmentation,
+  author={Tatiana Gaintseva},
+  title={RuCode Hand Segmentation},
+  year={2021},
+  url={https://www.kaggle.com/competitions/rucode-hand-segmentation/}
+}
 ```
 
 [Source](https://www.kaggle.com/competitions/rucode-hand-segmentation/)
--- src/settings.py
+++ src/settings.py
@@ -21,7 +21,7 @@ HIDE_DATASET = True  # set False when 100% sure about repo quality
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.Unknown()
-APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Entertainment()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Entertainment(is_used=False)]
 CATEGORY: Category = Category.Entertainment()
 
 CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation()]
